fda748163ed4dec456622b7531f656af2658989e
egor_follia <egorfollia@gmail.com>
2019-09-17 10:19:05 +0300

    Your Best Divisor submission got 20.00 points

---
+++ Fundamentals/Best_Divisor.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+
+int main()
+{
+  uint n;
+  std::cin >> n;
+
+  uint max = 0;
+  uint better = 0;
+  for( auto i = 1; i <= n; ++i)
+  {
+    if( n % i == 0)
+    {
+      uint sum = 0;
+      uint rem = 0;
+      uint j = i;
+      while( j != 0 )
+      {
+        rem = j % 10;
+        sum += rem;
+        j /= 10;
+      }
+
+      if ( max < sum )
+      {
+        max = sum;
+        better = i;
+      }
+    }
+  }
+
+  std::cout << better << std::endl;
+
+  return 0;
+}
